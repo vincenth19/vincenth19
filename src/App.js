6859ed165da30f1f5d1bd4988d0a48abd20e0915
vincenth19 <vincenthary19@gmail.com>
2021-07-05 10:54:55 +0800

    add tags, change icon colour

--- src/App.js
+++ src/App.js
@@ -10,13 +10,20 @@ import {
   Spacer,
   theme,
   Heading,
-  Divider
+  Divider,
+  Tag,
 } from '@chakra-ui/react';
 // import { ColorModeSwitcher } from './ColorModeSwitcher';
-import prof_pic from './prof_pic.jpeg'
-import virus from './Virus512.png'
-import {FaGithub, FaInstagram, FaBehance, FaLinkedinIn, FaFigma} from 'react-icons/fa'
-import {RiMailLine} from 'react-icons/ri'
+import prof_pic from './prof_pic.jpeg';
+import virus from './Virus512.png';
+import {
+  FaGithub,
+  FaInstagram,
+  FaBehance,
+  FaLinkedinIn,
+  FaFigma,
+} from 'react-icons/fa';
+import { RiMailLine } from 'react-icons/ri';
 
 function App() {
   return (
@@ -24,59 +31,137 @@ function App() {
       <Box minH="100vh" p={3} bg="blue.50">
         {/* <ColorModeSwitcher /> */}
         <Flex alignItems="center" minH="97vh">
-          <Stack bg="white" shadow="md" p={5} borderRadius={20} mx="auto" width={["99%", "98%", "80%", "50%"]} >
+          <Stack
+            bg="white"
+            shadow="md"
+            p={5}
+            borderRadius={20}
+            mx="auto"
+            width={['99%', '98%', '80%', '50%']}
+          >
             <Flex>
-              <Text color="gray.500" fontWeight="semibold">MY PROFILE</Text>
-              <Spacer/>
+              <Text color="gray.500" fontWeight="semibold">
+                MY PROFILE
+              </Text>
+              <Spacer />
               {/* <ColorModeSwitcher />  */}
             </Flex>
             <Box>
-              <Divider/>
-              <Stack direction={["column", "row"]} my={5} spacing={5} alignItems="center">
-                <Image src={prof_pic} boxSize="150px" borderRadius={100}/>
-                <Stack direction='column' textAlign={["center", "left", "left", "left"]}>
-                  <Heading as="h1" size="xl" color="gray.800">Vincent Haryadi</Heading>
+              <Divider />
+              <Stack
+                direction={['column', 'row']}
+                my={5}
+                spacing={5}
+                alignItems="center"
+              >
+                <Image src={prof_pic} boxSize="150px" borderRadius={100} />
+                <Stack
+                  direction="column"
+                  textAlign={['center', 'left', 'left', 'left']}
+                >
+                  <Heading as="h1" size="xl" color="gray.800">
+                    Vincent Haryadi
+                  </Heading>
                   <Text fontSize="md">{`Front-end Developer & UI/UX Designer`}</Text>
                   <Stack direction="row" spacing={5}>
-                    <Link href="https://www.linkedin.com/in/vincenth19/" target="_blank">
-                      <Text fontSize="2rem" color="#2466C3"><FaLinkedinIn/></Text>
+                    <Link
+                      href="https://www.linkedin.com/in/vincenth19/"
+                      target="_blank"
+                    >
+                      <Text fontSize="2rem" color="#gray.700">
+                        <FaLinkedinIn />
+                      </Text>
                     </Link>
                     <Link href="https://github.com/vincenth19" target="_blank">
-                      <Text fontSize="2rem" color="#0D1117"><FaGithub/></Text>
+                      <Text fontSize="2rem" color="#gray.700">
+                        <FaGithub />
+                      </Text>
                     </Link>
-                    <Link href="https://instagram.com/vincenth19" target="_blank">
-                      <Text fontSize="2rem" color="#000000"><FaInstagram/></Text>
+                    <Link
+                      href="https://instagram.com/vincenth19"
+                      target="_blank"
+                    >
+                      <Text fontSize="2rem" color="#gray.700">
+                        <FaInstagram />
+                      </Text>
                     </Link>
                     <Link href="mailto:vincenthary19@gmail.com" target="_blank">
-                      <Text fontSize="2rem" color="gray.700"><RiMailLine/></Text>
+                      <Text fontSize="2rem" color="gray.700">
+                        <RiMailLine />
+                      </Text>
                     </Link>
-                    <Link href="https://www.behance.net/vincentharyadi" target="_blank">
-                      <Text fontSize="2rem" color="#2857FF"><FaBehance/></Text>
+                    <Link
+                      href="https://www.behance.net/vincentharyadi"
+                      target="_blank"
+                    >
+                      <Text fontSize="2rem" color="#gray.700">
+                        <FaBehance />
+                      </Text>
                     </Link>
                   </Stack>
                 </Stack>
               </Stack>
-              <Divider/>
-              <Stack spacing={5} mt={[0,5]}>
-              <Text color="gray.500" fontWeight="semibold">MY PROJECTS</Text>
+              <Divider />
+              <Stack spacing={5} mt={[0, 5]}>
+                <Text color="gray.500" fontWeight="semibold">
+                  MY PROJECTS
+                </Text>
                 <Link href="https://covidtracker.pages.dev" target="_blank">
-                  <Box border="1px" borderColor="gray.300" p={5} borderRadius={10}>
+                  <Box
+                    border="1px"
+                    borderColor="gray.300"
+                    p={5}
+                    borderRadius={10}
+                  >
                     <Stack direction="row" spacing={5}>
-                      <Image src={virus} boxSize="70px" borderRadius={100}/>
+                      <Image src={virus} boxSize="70px" borderRadius={100} />
                       <Stack>
-                        <Text fontWeight="bold" fontSize="1.2rem">CovidTracker</Text>
+                        <Text fontWeight="bold" fontSize="1.2rem">
+                          CovidTracker
+                        </Text>
+                        <Stack direction="row">
+                          <Tag bg="blue.50" color="blue.700">
+                            ReactJS
+                          </Tag>
+                          <Tag bg="blue.50" color="blue.700">
+                            Chakra UI
+                          </Tag>
+                        </Stack>
                         <Text>Simple COVID-19 dashboard for Indonesia</Text>
                       </Stack>
                     </Stack>
                   </Box>
                 </Link>
-                <Link href="https://www.figma.com/file/k9KnpIAi7BoJqOQAXfR90X/Reminder-2.0?node-id=0%3A1" target="_blank">
-                  <Box border="1px" borderColor="gray.300" p={5} borderRadius={10}>
+                <Link
+                  href="https://www.figma.com/file/k9KnpIAi7BoJqOQAXfR90X/Reminder-2.0?node-id=0%3A1"
+                  target="_blank"
+                >
+                  <Box
+                    border="1px"
+                    borderColor="gray.300"
+                    p={5}
+                    borderRadius={10}
+                  >
                     <Stack direction="row" spacing={5}>
-                      <Text fontSize="3.6em" color="#A359FF"><FaFigma/></Text>
+                      <Text fontSize="3.6em" color="#A359FF">
+                        <FaFigma />
+                      </Text>
                       <Stack>
-                        <Text fontWeight="bold" fontSize="1.2rem">Mask Reminder App Design</Text>
-                        <Text>Mobile application design that is used to remind user to wear mask.</Text>
+                        <Text fontWeight="bold" fontSize="1.2rem">
+                          Mask Reminder App Design
+                        </Text>
+                        <Stack direction="row">
+                          <Tag bg="blue.50" color="blue.700">
+                            Figma
+                          </Tag>
+                          <Tag bg="blue.50" color="blue.700">
+                            UI/UX
+                          </Tag>
+                        </Stack>
+                        <Text>
+                          Mobile application design that is used to remind user
+                          to wear mask.
+                        </Text>
                       </Stack>
                     </Stack>
                   </Box>
@@ -84,7 +169,6 @@ function App() {
               </Stack>
             </Box>
           </Stack>
-          
         </Flex>
       </Box>
     </ChakraProvider>
