516370618c8419a16dc8fa1e2de8f8bd82462e8d
vincenth19 <vincenthary19@gmail.com>
2023-02-21 00:40:41 +0700

    fix: add default export in api

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,5 +1,17 @@
 // Next.js Edge API Routes: https://nextjs.org/docs/api-routes/edge-api-routes
 
+import { NextRequest } from 'next/server';
+
 export const config = {
 	runtime: 'experimental-edge',
 };
+
+// default export function need to be here so I can deploy to Cloudflare Page
+export default async function handler(req: NextRequest): Promise<Response> {
+	return new Response(JSON.stringify({ name: 'John Doe' }), {
+		status: 200,
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	});
+}
